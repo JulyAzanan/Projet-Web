029f1786f1e1982fbd69d4a2862f86b05082621b
oganexon <oganexon@protonmail.com>
2021-04-02 18:09:33 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+  <!-- Navigation -->
   <nav uk-sticky class="uk-navbar-container uk-margin" uk-navbar>
-    <div class="uk-navbar-left test-overlay">
+    <div class="uk-navbar-left search-overlay">
+      <!-- Logo -->
       <div class="uk-navbar-item">
         <router-link to="/">
           <img src="@/assets/logo_simple.png" width="50" alt="" />
@@ -8,170 +10,215 @@
         </router-link>
       </div>
     </div>
-    <div class="uk-navbar-center test-overlay">
+    <div class="uk-navbar-center search-overlay">
+      <!-- Routes principales -->
       <ul class="uk-navbar-nav">
-        <li :class="{'uk-active': $route.path === '/users'}"><router-link to="/users">utilisateurs</router-link></li>
-        <li :class="{'uk-active': $route.path === '/projects'}"><router-link to="/users">projets</router-link></li>
-        <li :class="{'uk-active': $route.path === '/about'}"><router-link to="/about">à propos</router-link></li>
-        <li>
-          <a href="#">Parent</a>
-          <div class="uk-navbar-dropdown">
-            <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li class="uk-active"><a href="#">Active</a></li>
-              <li class="uk-parent">
-                <a href="#">Parent</a>
-                <ul class="uk-nav-sub">
-                  <li><a href="#">Sub item</a></li>
-                  <li><a href="#">Sub item</a></li>
-                </ul>
-              </li>
-              <li class="uk-nav-header">Header</li>
-              <li>
-                <a href="#"
-                  ><span
-                    class="uk-margin-small-right"
-                    uk-icon="icon: table"
-                  ></span>
-                  Item</a
-                >
-              </li>
-              <li>
-                <a href="#"
-                  ><span
-                    class="uk-margin-small-right"
-                    uk-icon="icon: thumbnails"
-                  ></span>
-                  Item</a
-                >
-              </li>
-              <li class="uk-nav-divider"></li>
-              <li>
-                <a href="#"
-                  ><span
-                    class="uk-margin-small-right"
-                    uk-icon="icon: trash"
-                  ></span>
-                  Item</a
-                >
-              </li>
-            </ul>
-          </div>
+        <li :class="{ 'uk-active': $route.path === '/users' }">
+          <router-link to="/users">utilisateurs</router-link>
         </li>
-        <li>
-          <a href="#">Content</a>
-          <div class="uk-navbar-dropdown">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
+        <li :class="{ 'uk-active': $route.path === '/projects' }">
+          <router-link to="/projects">projets</router-link>
         </li>
-        <li><a href="#">Item</a></li>
-        <li><a href="#">Item</a></li>
-        <li><a href="#">Item</a></li>
+        <li :class="{ 'uk-active': $route.path === '/about' }">
+          <router-link to="/about">à propos</router-link>
+        </li>
       </ul>
-      <div class="uk-navbar-item">
-        <div>Some <a href="#">Link</a></div>
-      </div>
     </div>
-    <div class="uk-navbar-right test-overlay">
+    <div class="uk-navbar-right search-overlay">
       <ul class="uk-navbar-nav">
-        <li>
+        <li class="uk-active">
           <a
             class="uk-navbar-toggle"
             uk-search-icon
-            uk-toggle="target: .test-overlay; animation: uk-animation-fade"
-            href="#"
+            uk-toggle="target: .search-overlay; animation: uk-animation-fade"
           ></a>
         </li>
-        <li v-if="logged">
-          <a href="#">Steel</a>
+      </ul>
+      <!-- Actions rapide -->
+      <div v-if="logged" class="uk-navbar-item">
+        <ul class="uk-iconnav">
+          <li>
+            <a
+              href="#"
+              uk-icon="icon: plus"
+              uk-tooltip="title: Nouveau projet; pos: bottom; delay: 200"
+            ></a>
+          </li>
+          <li>
+            <a uk-icon="icon: thumbnails"></a>
+            <div uk-dropdown="pos: bottom-justify">
+              <ul class="uk-nav uk-dropdown-nav">
+                <li class="uk-nav-header">Projets récents</li>
+                <li>
+                  <a href="#">
+                    <span
+                      class="uk-margin-small-right"
+                      uk-icon="icon: folder"
+                    ></span>
+                    Projet 3615
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span
+                      class="uk-margin-small-right"
+                      uk-icon="icon: folder"
+                    ></span>
+                    Nier: Automata
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span
+                      class="uk-margin-small-right"
+                      uk-icon="icon: folder"
+                    ></span>
+                    Daft Punk
+                  </a>
+                </li>
+                <li class="uk-nav-divider"></li>
+                <li>
+                  <a href="#">
+                    <span
+                      class="uk-margin-small-right"
+                      uk-icon="icon: lock"
+                    ></span>
+                    Foo
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span
+                      class="uk-margin-small-right"
+                      uk-icon="icon: lock"
+                    ></span>
+                    Bar
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a
+              href="#"
+              uk-icon="icon: bell"
+              uk-tooltip="title: Notifications; pos: bottom; delay: 200"
+              ><span class="uk-badge">2</span></a
+            >
+          </li>
+        </ul>
+      </div>
+      <ul class="uk-navbar-nav">
+        <!-- Profil utilisateur -->
+        <li v-if="logged" class="uk-active">
+          <a class="username">{{ username }} </a>
           <div class="uk-navbar-dropdown">
             <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li class="uk-nav-header">Active</li>
-              <li class="uk-parent">
-                <a href="#">Paramètres</a>
-                <a href="#">Mon profil</a>
-                <ul class="uk-nav-sub">
-                  <li><a href="#">Mes projets</a></li>
-                  <li><a href="#">Mes amis</a></li>
-                </ul>
-              </li>
-              <li class="uk-nav-header">Header</li>
               <li>
-                <a href="#"
-                  ><span
+                <a href="#">
+                  <span
                     class="uk-margin-small-right"
-                    uk-icon="icon: table"
+                    uk-icon="icon: user"
                   ></span>
-                  Item</a
-                >
+                  Mon profil
+                </a>
               </li>
               <li>
-                <a href="#"
-                  ><span
+                <a href="#">
+                  <span
                     class="uk-margin-small-right"
-                    uk-icon="icon: thumbnails"
+                    uk-icon="icon: album"
                   ></span>
-                  Item</a
-                >
+                  Mes projets
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span
+                    class="uk-margin-small-right"
+                    uk-icon="icon: users"
+                  ></span>
+                  Mes amis
+                </a>
               </li>
               <li class="uk-nav-divider"></li>
               <li>
-                <a href="#"
-                  ><span
+                <a href="#">
+                  <span
                     class="uk-margin-small-right"
-                    uk-icon="icon: trash"
+                    uk-icon="icon: settings"
                   ></span>
-                  Déconnexion</a
-                >
+                  Paramètres
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span
+                    class="uk-margin-small-right"
+                    uk-icon="icon: sign-out"
+                  ></span>
+                  Déconnexion
+                </a>
               </li>
             </ul>
           </div>
         </li>
         <li v-else>
-          <a href="#">Se Connecter</a>
+          <router-link to="/login">
+            <span class="uk-margin-small-right" uk-icon="icon: sign-in"></span>
+            Se connecter
+          </router-link>
         </li>
       </ul>
     </div>
-    <div class="uk-navbar-left uk-flex-1 test-overlay" hidden>
+    <!-- Overlay de recherche -->
+    <div class="uk-navbar-left uk-flex-1 search-overlay" hidden>
       <div class="uk-navbar-item uk-width-expand">
         <form class="uk-search uk-search-navbar uk-width-1-1">
           <input
             class="uk-search-input"
             type="search"
-            placeholder="Search"
+            placeholder="Rechercher..."
             autofocus
           />
         </form>
       </div>
-
       <a
         class="uk-navbar-toggle"
         uk-close
-        uk-toggle="target: .test-overlay; animation: uk-animation-fade"
-        href="#"
+        uk-toggle="target: .search-overlay; animation: uk-animation-fade"
       ></a>
     </div>
   </nav>
+  <!-- Corps -->
   <div class="uk-container">
     <router-view />
   </div>
+  <!-- Pied de page -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup(props) {
+  setup() {
     const logged = true;
+    const username = "Steel";
     return {
       logged,
+      username,
     };
   },
 });
 </script>
 
-
 <style lang="scss">
 @import "@/styles/variables.scss";
 </style>
 
+<style lang="scss" scoped>
+.username {
+  text-transform: none;
+  font-weight: bold;
+}
+</style>
