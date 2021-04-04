843c6efcfc0d36e4470f70c4e989c80fb89436e2
JulyAzanan <samuel.benhaim.lyc@gmail.com>
2021-04-04 13:59:59 +0200

    Project cards (summary of a project)

---
+++ src/components/App/ProjectCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div>
+      <div class="uk-card uk-card-default uk-card-hover">
+        <div class="uk-card-badge">
+          <span
+            v-if="!priv"
+            class="uk-margin-small-right"
+            uk-icon="icon: users"
+            uk-tooltip="Public"
+          ></span>
+          <span
+            v-else
+            class="uk-margin-small-right"
+            uk-icon="icon: lock"
+            uk-tooltip="Privé"
+          ></span>
+        </div>
+        <div class="uk-card-body">
+          <p></p>
+          <h3 class="uk-card-title">{{project}}</h3>
+          <p>Dernière mise à jour le : {{ updatedAt }}</p>
+          <p>Créé par : {{ author }}</p>
+          <router-link
+            :to="{ path: '/-/' + username + '/' + project + '/blob/' + branch }"
+          >
+            Branche principale : {{ branch }}
+          </router-link>
+          <router-link
+            :to="{ path: '/-/' + username + '/' + project }"
+            uk-margin
+            class="uk-button uk-button-default"
+          >
+            Voir le projet
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    priv: Boolean,
+    updatedAt: String,
+    author: String,
+    username: String,
+    project: String,
+    branch: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
