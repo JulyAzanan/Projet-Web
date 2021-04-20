d59e24b8fd5bd9f12a884572ab1fd89ca584ccb1
oganexon <oganexon@protonmail.com>
2021-04-20 22:27:56 +0200

    API ts

--- src/api/branch.ts
+++ src/api/branch.ts
@@ -1,13 +1,67 @@
 import sleep from "./sleep"
 
-interface Result {
+interface BaseResult {
   updatedAt: Date,
+  createdAt: Date,
 }
 
-export async function find(user?: string, project?: string, branch?: string): Promise<Result | null> {
-  if (user == undefined || project == undefined || branch == undefined) throw null;
+export async function add(_author: string, _project: string, _branch: string): Promise<void> {
+  // TODO
+  return;
+}
+
+export async function remove(_author: string, _project: string, _branch: string): Promise<void> {
+  // TODO
+  return;
+}
+
+export async function rename(_author: string, _project: string, _branch: string, _newBranchName: string): Promise<void> {
+  // TODO
+  return;
+}
+
+export interface FindResult extends BaseResult {
+  lastCommit?: string
+}
+
+export async function find(user?: string, project?: string, branch?: string): Promise<FindResult | null> {
+  if (user == null || project == null || branch == null) return null;
   await sleep(500);
   return {
+    lastCommit: "8f5e91",
     updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
   }
+}
+
+export interface FetchAllFromResult extends BaseResult {
+  branch: string
+}
+
+export async function fetchAllFrom(first: number, after: number, author?: string, project?: string): Promise<FetchAllFromResult[]> {
+  if (after < 0) throw new Error("Negative index (after)");
+  if (first <= 0) throw new Error("Negative index (first)");
+  if (author == null || project == null) return [];
+  await sleep(500); // TODO
+  return [{
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    branch: "main",
+  },
+  {
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    branch: "dev",
+  },
+  {
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    branch: "prod",
+  }];
+}
+
+export async function countFrom(author?: string, project?: string): Promise<number> {
+  if (author == null || project == null) return 0;
+  await sleep(500); // TODO
+  return 5;
 }
--- src/api/project.ts
+++ src/api/project.ts
@@ -1,23 +1,138 @@
 import sleep from "./sleep"
 
-export interface Result {
-  contributors: string[],
-  mainBranch: string,
-  description: string,
+interface BaseResult {
+  private: boolean,
   updatedAt: Date,
   createdAt: Date,
+  description: string,
 }
 
-export async function find(user?: string, project?: string): Promise<Result | null> {
-  if (user == undefined || project == undefined) throw null;
-  await sleep(500);
-  const foo = false;
-  if (foo) return null;
+export async function add(_author: string, _project: string, _isPrivate: boolean, _description: string): Promise<void> {
+  // TODO
+  return;
+}
+
+export async function remove(_author: string, _project: string): Promise<void> {
+  // TODO
+  return;
+}
+
+export interface FindResult extends BaseResult {
+  contributors: string[],
+  mainBranch: string,
+}
+
+export async function find(user?: string, project?: string): Promise<FindResult | null> {
+  if (user == null || project == null) return null;
+  await sleep(500); // TODO
   return {
+    private: false,
     contributors: ["Steel", "July", "Michel"],
     mainBranch: "main",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque turpis eu libero elementum, mattis egestas nisi scelerisque. Nulla facilisi. Pellentesque lacinia felis non leo scelerisque tristique. Suspendisse bibendum quam quis ullamcorper lacinia. Ut nec semper enim, vitae efficitur felis. Curabitur scelerisque dignissim metus, at sagittis tortor scelerisque sit amet. Donec eu tincidunt justo.",
     updatedAt: new Date(),
-    createdAt: new Date(),
-  }
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+  };
+}
+
+export interface FetchAllResult extends BaseResult {
+  author: string,
+  project: string,
+}
+
+export async function fetchAll(first: number, after: number): Promise<FetchAllResult[]> {
+  if (after < 0) throw new Error("Negative index (after)");
+  if (first <= 0) throw new Error("Negative index (first)");
+  await sleep(500); // TODO
+  return [{
+    private: true,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    author: "July",
+    project: "Nier: Automata",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    author: "Steel",
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    author: "Steel",
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    author: "Steel",
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    author: "Steel",
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }]
+}
+
+export interface FetchAllFromResult extends BaseResult {
+  project: string,
+}
+
+export async function fetchAllFrom(first: number, after: number, user?: string): Promise<FetchAllFromResult[]> {
+  if (after < 0) throw new Error("Negative index (after)");
+  if (first <= 0) throw new Error("Negative index (first)");
+  if (user == null) return [];
+  await sleep(500); // TODO
+  return [{
+    private: true,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    project: "Nier: Automata",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }, {
+    private: false,
+    updatedAt: new Date(),
+    createdAt: new Date(new Date().getTime() - 34 * 24 * 60 * 60 * 1011),
+    project: "Daft Punk",
+    description: "Lorem ipsum dolor sit amet."
+  }];
+}
+
+export async function count(): Promise<number> {
+  await sleep(500); // TODO
+  return 5;
+}
+
+export async function countFrom(user?: string): Promise<number> {
+  if (user == null) return 0;
+  await sleep(500); // TODO
+  return 5;
 }
