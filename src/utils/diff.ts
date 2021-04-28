edb17afcb0f82ba9ef0e749be5bee7b632a0e31a
oganexon <oganexon@protonmail.com>
2021-04-28 22:22:43 +0200

    optimisation mesure

---
+++ src/utils/diff.ts
@@ -0,0 +1,210 @@
+import format from "xml-formatter";
+
+interface Note {
+  element: Element;
+  raw: string;
+}
+
+const colors = {
+  added: "#00FF00",
+  removed: "#FF0000",
+  modified: "#AAAA00"
+}
+
+const finalSequence: Note = {
+  element: document.createElement("lcs-end"),
+  raw: "",
+};
+
+function removeAttributes(element: Element): Element {
+  const el = element.cloneNode(true) as Element;
+  while (el.attributes.length > 0)
+    el.removeAttribute(el.attributes[0].name);
+  return el;
+}
+
+function equals(a?: Note, b?: Note) {
+  return a && b && a.raw === b.raw;
+}
+
+function longestCommonSubsequence(c: Note[], d: Note[]): Note[] {
+  // common final sequence
+  c.push(finalSequence);
+  d.push(finalSequence);
+
+  const matrix = Array.from(
+    new Array(c.length + 1),
+    () => new Array(d.length + 1)
+  );
+
+  function backtrack(c: Note[], d: Note[], x: number, y: number): Note[] {
+    if (x === 0 || y === 0) return [];
+    return equals(c[x - 1], d[y - 1])
+      ? backtrack(c, d, x - 1, y - 1).concat(c[x - 1]) // x-1, y-1
+      : matrix[x][y - 1] > matrix[x - 1][y]
+        ? backtrack(c, d, x, y - 1)
+        : backtrack(c, d, x - 1, y);
+  }
+
+  for (let i = 0; i < c.length; i++) {
+    matrix[i][0] = 0;
+  }
+  for (let j = 0; j < d.length; j++) {
+    matrix[0][j] = 0;
+  }
+  for (let i = 1; i <= c.length; i++) {
+    for (let j = 1; j <= d.length; j++) {
+      matrix[i][j] = equals(c[i - 1], d[j - 1])
+        ? matrix[i - 1][j - 1] + 1 // i-1, j-1
+        : Math.max(matrix[i][j - 1], matrix[i - 1][j]);
+    }
+  }
+
+  const result = backtrack(c, d, c.length, d.length);
+  // remove final sequence
+  c.pop();
+  d.pop();
+  result.pop();
+  return result;
+}
+
+type NoteArray = {
+  elements: Element[],
+  type: "properties"
+} | {
+  elements: Element[],
+  type: "symbols"
+}
+
+function splitMeasure(measure: Element): NoteArray[] {
+  if (measure.children.length === 0) return [];
+  const notes: NoteArray[] = [];
+  let mode = "init";
+  for (const element of measure.children) {
+    if (element.tagName === "note") {
+      if (mode !== "symbols") notes.push({
+        elements: [],
+        type: "symbols",
+      });
+      mode = "symbols";
+    } else {
+      if (mode !== "properties") notes.push({
+        elements: [],
+        type: "properties",
+      });
+      mode = "properties";
+    }
+    notes[notes.length - 1].elements.push(element);
+  }
+  return notes;
+}
+
+function markNotesAdded(notes: Element[], diff: Element): void {
+  for (const element of notes) {
+    if (element.tagName === "note") {
+      const el = element.cloneNode(true) as Element;
+      el.setAttribute("color", colors.added);
+      diff.appendChild(el);
+    }
+  }
+}
+
+function markNotesRemoved(notes: Element[], diff: Element): void {
+  for (const element of notes) {
+    if (element.tagName === "note") {
+      const el = element.cloneNode(true) as Element;
+      el.setAttribute("color", colors.removed);
+      diff.appendChild(el);
+    }
+  }
+}
+
+export function measureDiff(measure_a: Element, measure_b: Element, diff: Element): void {
+  while (diff.firstChild) {
+    diff.firstChild.remove();
+  }
+
+  const split_a = splitMeasure(measure_a);
+  const split_b = splitMeasure(measure_b);
+
+  console.log(split_a, split_b)
+
+  let j = 0;
+  for (let i = 0; i < split_a.length; i++) {
+    while(split_b[j] !== undefined && split_b[j].type !== "symbols") j++;
+    if(split_a[i].type === "properties") {
+      diff.append(...split_a[i].elements);
+    } else {
+      if (split_b[j] !== undefined) {
+        noteDiff(split_a[i].elements, split_b[j].elements, diff);
+      } else {
+        markNotesAdded(split_a[i].elements, diff)
+      }
+      j++;
+    }
+  }
+  for (let i = j; i < split_b.length; i++) {
+    if (split_b[i].type === "properties") {
+      diff.append(...split_b[i].elements);
+    } else {
+      markNotesRemoved(split_b[i].elements, diff)
+    }
+  }
+}
+
+function noteDiff(notes_a: Element[], notes_b: Element[], notes: Element): void {
+  const xml_a: Note[] = []
+  const xml_b: Note[] = []
+  for (const el of notes_a) {
+    xml_a.push({
+      element: el,
+      raw: format(removeAttributes(el).outerHTML)
+    })
+  }
+  for (const el of notes_b) {
+    xml_b.push({
+      element: el,
+      raw: format(removeAttributes(el).outerHTML)
+    })
+  }
+
+  const LCS = longestCommonSubsequence(xml_a, xml_b);
+  let actualIndex = 0;
+  let baseIndex = 0;
+
+  for (let i = 0; i <= LCS.length; i++) {
+    while (
+      !equals(LCS[i], xml_b[baseIndex]) &&
+      baseIndex < notes_b.length
+    ) {
+      if (
+        !equals(LCS[i], xml_a[actualIndex]) &&
+        actualIndex < notes_a.length
+      ) {
+        const note = notes_a[actualIndex].cloneNode(true) as Element;
+        note.setAttribute("color", colors.modified);
+        notes.appendChild(note);
+        actualIndex++;
+      } else {
+        const note = notes_b[baseIndex].cloneNode(true) as Element;
+        note.setAttribute("color", colors.removed);
+        notes.appendChild(note);
+      }
+      baseIndex++;
+    }
+    while (
+      !equals(LCS[i], xml_a[actualIndex]) &&
+      actualIndex < notes_a.length
+    ) {
+      const note = notes_a[actualIndex].cloneNode(true) as Element;
+      note.setAttribute("color", colors.added);
+      notes.appendChild(note);
+      actualIndex++;
+    }
+    if (LCS[i] !== undefined) {
+      notes.appendChild(LCS[i].element.cloneNode(true));
+    }
+    baseIndex++;
+    actualIndex++;
+  }
+}
--- src/views/Pulls.vue
+++ src/views/Pulls.vue
@@ -7,80 +7,10 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
-import format from "xml-formatter"
 import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
-// import score1 from "@/scores/score1";
 import score1 from "@/scores/E12_intermission.musicxml";
 import score2 from "@/scores/E12_intermission-1.musicxml";
-
-export function longestCommonSubsequence(
-  c: Element[],
-  d: Element[]
-): Element[] {
-  // common final sequence
-  // console.log(c)
-  // console.log(d)
-  for (const el of c) {
-    while(el.attributes.length > 0)
-    el.removeAttribute(el.attributes[0].name);
-    console.log(format(el.outerHTML))
-  }
-  for (const el of d) {
-    while(el.attributes.length > 0)
-    el.removeAttribute(el.attributes[0].name);
-    console.log(format(el.outerHTML))
-  }
-  // console.log(c[0], d[0])
-  // console.log(c[0].isEqualNode(d[0]))
-
-  c.push(document.createElement("div"));
-  d.push(document.createElement("div"));
-
-  const matrix = Array.from(
-    new Array(c.length + 1),
-    () => new Array(d.length + 1)
-  );
-
-  // console.log(matrix)
-
-  function backtrack(
-    c: Element[],
-    d: Element[],
-    x: number,
-    y: number
-  ): Element[] {
-    if (x === 0 || y === 0) return [];
-    return format(c[x - 1].outerHTML) === format(d[y - 1].outerHTML)
-      ? backtrack(c, d, x - 1, y - 1).concat(c[x - 1]) // x-1, y-1
-      : matrix[x][y - 1] > matrix[x - 1][y]
-      ? backtrack(c, d, x, y - 1)
-      : backtrack(c, d, x - 1, y);
-  }
-
-  for (let i = 0; i < c.length; i++) {
-    matrix[i][0] = 0;
-  }
-  for (let j = 0; j < d.length; j++) {
-    matrix[0][j] = 0;
-  }
-  for (let i = 1; i <= c.length; i++) {
-    for (let j = 1; j <= d.length; j++) {
-      matrix[i][j] =
-        format(c[i - 1].outerHTML) === format(d[j - 1].outerHTML)
-          ? matrix[i - 1][j - 1] + 1 // i-1, j-1
-          : Math.max(matrix[i][j - 1], matrix[i - 1][j]);
-    }
-  }
-
-  console.log(matrix)
-
-  const result = backtrack(c, d, c.length, d.length);
-  // remove final sequence
-  c.pop();
-  d.pop();
-  result.pop();
-  return result;
-}
+import { measureDiff } from "@/utils/diff";
 
 export default defineComponent({
   props: {
@@ -93,89 +23,19 @@ export default defineComponent({
 
     const actual = parser.parseFromString(score2, "text/xml");
     const base = parser.parseFromString(score1, "text/xml");
-    const diff = parser.parseFromString(score1, "text/xml");
+    const diff = parser.parseFromString(score2, "text/xml");
 
     const parts_a = actual.getElementsByTagName("part");
     const parts_b = base.getElementsByTagName("part");
     const parts_d = diff.getElementsByTagName("part");
-    for (let i_p = 0; i_p < /* parts_d.length */1; i_p++) {
+    for (let i_p = 0; i_p < /* parts_d.length */ 1; i_p++) {
       const measures_a = parts_a[i_p].getElementsByTagName("measure");
       const measures_b = parts_b[i_p].getElementsByTagName("measure");
       const measures_d = parts_d[i_p].getElementsByTagName("measure");
-      for (let i_m = 0; i_m < /* measures_d.length */1; i_m++) {
-        const notes_a = measures_a[i_m].children;
-        const notes_b = measures_b[i_m].children;
-        const notes = measures_d[i_m];
-
-        while (notes.firstChild) {
-          notes.firstChild.remove();
-        }
-        const LCS = longestCommonSubsequence([...notes_a], [...notes_b]);
-        let actualIndex = 0;
-        let baseIndex = 0;
-
-        for (let i = 0; i <= LCS.length; i++) {
-          while (
-            LCS[i] !== undefined &&
-            format(LCS[i].outerHTML) !== format(notes_b[baseIndex].outerHTML) &&
-            baseIndex < notes_b.length
-          ) {
-            if (
-              format(LCS[i].outerHTML) !== format(notes_a[actualIndex].outerHTML) &&
-              actualIndex < notes_a.length
-            ) {
-              // diff.push(compare_(actual[actualIndex], base[baseIndex]));
-              // modified
-              const note = notes_a[actualIndex].cloneNode(true) as Element;
-              note.setAttribute("color", "#FFAA00");
-              notes.appendChild(note);
-              actualIndex++;
-            } else {
-              const note = notes_b[baseIndex].cloneNode(true) as Element;
-              note.setAttribute("color", "#FF0000");
-            notes.appendChild(note);
-            }
-            baseIndex++;
-          }
-          while (
-            LCS[i] !== undefined &&
-            format(LCS[i].outerHTML) !== format(notes_a[actualIndex].outerHTML) &&
-            actualIndex < notes_a.length
-          ) {
-            const note = notes_a[actualIndex].cloneNode(true) as Element;
-            note.setAttribute("color", "#00FF00");
-            notes.appendChild(note);
-            actualIndex++;
-          }
-          if (LCS[i] !== undefined) {
-            notes.appendChild(LCS[i].cloneNode(true)); //fiks //Nani
-          }
-          baseIndex++;
-          actualIndex++;
-        }
+      for (let i_m = 0; i_m < measures_d.length; i_m++) {
+        measureDiff(measures_a[i_m], measures_b[i_m], measures_d[i_m])
       }
     }
-    // console.log(actual.getElementsByTagName("part")[0].getElementsByTagName("measure")[0])
-    // console.log(base.getElementsByTagName("part")[0].getElementsByTagName("measure")[0])
-    // const notes$ = actual.getElementsByTagName("part")[0].getElementsByTagName("measure")[0].children
-    // const notes_ = base.getElementsByTagName("part")[0].getElementsByTagName("measure")[0].children
-    // console.log(notes$)
-    // console.log(diff);
-    // console.log(measure);
-    // const notes = measure.getElementsByTagName("note");
-    // const note = notes_[0]
-    // const note0 = notes_[0].cloneNode(true);
-    // notes[0].setAttribute("color", "#FF0000");
-    // console.log(notes[0].isEqualNode(notes[1]));
-    // console.log(notes_[3])
-    // console.log(notes$[3])
-    // console.log(format(notes_[3].outerHTML) === format(notes_[4].outerHTML));
-    // console.log(format(notes$[3].outerHTML)=== format(notes$[4].outerHTML));
-    // console.log(format(notes$[3].outerHTML)=== format(notes_[3].outerHTML));
-    // console.log(notes_[3].isSameNode(notes_[4]));
-    // console.log(notes$[3].isSameNode(notes$[4]));
-    // console.log(notes$[3].isSameNode(notes_[3]));
-    // console.log(note0, notes[0], notes[1]);
 
     async function loadScore() {
       const osmd = new OpenSheetMusicDisplay(osmdContainer.value!, {
